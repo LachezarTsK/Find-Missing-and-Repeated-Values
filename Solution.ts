07e74cd7be2f6b226167743009ac02b371359718
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-03-07 17:58:04 +0100

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,26 @@
+
+function findMissingAndRepeatedValues(matrix: number[][]): number[] {
+    const rows = matrix.length;
+    const columns = matrix[0].length;
+    const numberOfElements = rows * columns;
+
+    let repeatedValue = 0;
+    let sumValuesInMatrix = 0;
+    const visited: boolean[] = new Array(numberOfElements + 1).fill(false);
+
+    for (let r = 0; r < rows; ++r) {
+        for (let c = 0; c < columns; ++c) {
+            const value = matrix[r][c];
+            if (visited[value]) {
+                repeatedValue = value;
+            }
+            sumValuesInMatrix += value;
+            visited[value] = true;
+        }
+    }
+
+    const sumValuesInRange = (numberOfElements + 1) * numberOfElements / 2;
+    const missingValue = sumValuesInRange - sumValuesInMatrix + repeatedValue;
+
+    return [repeatedValue, missingValue];
+};
